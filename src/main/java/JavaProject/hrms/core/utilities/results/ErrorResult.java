41125c5f3e6e931b859bcce0788507d28c2e91a2
Metehan Özalp <metehanozalp@hotmail.com>
2021-05-23 17:01:16 +0300

    results package writhed

---
+++ src/main/java/JavaProject/hrms/core/utilities/results/ErrorResult.java
@@ -0,0 +1,13 @@
+package JavaProject.hrms.core.utilities.results;
+
+public class ErrorResult extends Result {
+
+	public ErrorResult() {
+		super(false);
+	}
+
+	public ErrorResult(String message) {
+		super(false, message);
+	}
+
+}
