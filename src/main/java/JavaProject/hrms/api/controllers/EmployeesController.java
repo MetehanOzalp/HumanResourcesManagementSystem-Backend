790d636ed61e85bf46afad02376eb32a11e00e43
Metehan Özalp <metehanozalp@hotmail.com>
2021-06-07 00:30:34 +0300

    added cross origin

--- src/main/java/JavaProject/hrms/api/controllers/EmployeesController.java
+++ src/main/java/JavaProject/hrms/api/controllers/EmployeesController.java
@@ -3,6 +3,7 @@ package JavaProject.hrms.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import JavaProject.hrms.entities.concretes.Employee;
 
 @RestController
 @RequestMapping("/api/employees/")
+@CrossOrigin
 public class EmployeesController {
 
 	private EmployeeService employeeService;
