1d4227385af805699323bba8f491d3222bf17b7e
Metehan Özalp <metehanozalp@hotmail.com>
2021-06-25 01:37:21 +0300

    cross origin added

--- src/main/java/JavaProject/hrms/api/controllers/CurriculumVitaesController.java
+++ src/main/java/JavaProject/hrms/api/controllers/CurriculumVitaesController.java
@@ -1,6 +1,7 @@
 package JavaProject.hrms.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import JavaProject.hrms.entities.concretes.CurriculumVitae;
 
 @RestController
 @RequestMapping("/api/curriculumVitaes/")
+@CrossOrigin
 public class CurriculumVitaesController {
 
 	private CurriculumVitaeService curriculumVitaeService;
